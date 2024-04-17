1276f4d86c366ef9fc8dec00f14cf89dfb60f2ae
markdev <109904787+MarkPavlenko@users.noreply.github.com>
2024-04-17 19:18:51 +0300

    Initial commit

---
+++ script.py
@@ -0,0 +1,10 @@
+import webview
+
+webview.create_window("NoWIFI-ChatGPT", "https://chat.openai.com/",)
+webview.start()
+
+startUp = print("started")
+if startUp:
+    print("started")
+else:
+    print("error")
